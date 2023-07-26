6cd352effa734cb49921ea1ae8a7e6707b21216e
CalebTek <olawale.odeyemi@decagon.dev>
2023-07-26 22:44:48 +0100

    Added Health class and enums

---
+++ Palmfit/Palmfit.Data/Entities/Health.cs
@@ -0,0 +1,16 @@
+﻿using Palmfit.Data.EntityEnums;
+
+namespace Palmfit.Data.Entities
+{
+    public class Health
+    {
+        public int AppUserId { get; set; }
+        public double Height { get; set; }
+        public HeightUnit HeightUnit { get; set; }
+        public double Weight { get; set; }
+        public WeightUnit WeightUnit { get; set; }
+        public BloodGroup BloodGroup { get; set; }
+        public GenoType GenoType { get; set; }
+        public AppUser AppUser { get; set; }
+    }
+}
