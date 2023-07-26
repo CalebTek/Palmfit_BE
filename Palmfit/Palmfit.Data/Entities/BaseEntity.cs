4db707f9b94c0f89c96e9cafcfad94fa0b79ad9c
John Kehinde <kehindejohn018@gmail.com>
2023-07-26 20:55:32 +0100

    Added base entity

---
+++ Palmfit/Palmfit.Data/Entities/BaseEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace Palmfit.Data.Entities
+{
+    public class BaseEntity
+    {
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public DateTime DateDeleted { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
