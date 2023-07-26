d383d699254fb333cc178a31755ea2b1f34dc467
CalebTek <olawale.odeyemi@decagon.dev>
2023-07-26 23:07:13 +0100

    Add Reviews Entity

---
+++ Palmfit/Palmfit.Data/Entities/Reviews.cs
@@ -0,0 +1,12 @@
+﻿namespace Palmfit.Data.Entities
+{
+    public class Reviews
+    {
+        public DateTime Date { get; set; }
+        public string Comment { get; set; }
+        public int Rating { get; set; }
+        public string Verdict { get; set; } // There is a comment property, why verdict?
+        public int AppUserId { get; set; }
+        public AppUser AppUser { get; set; }
+    }
+}
