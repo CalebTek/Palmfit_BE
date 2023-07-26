f469d5a4ee10eeaa12909187f097b1170bb05a3b
CalebTek <olawale.odeyemi@decagon.dev>
2023-07-26 23:03:51 +0100

    Add Notification Entity

--- Palmfit/Palmfit.Data/Entities/Notifications.cs
+++ Palmfit/Palmfit.Data/Entities/Notifications.cs
@@ -2,5 +2,11 @@
 {
     public class Notifications
     {
+        public DateTime Date { get; set; }
+        public bool IsRead { get; set; }
+        public string Url { get; set; }
+        public string Message { get; set; }
+        public int AppUserId { get; set; }
+        public AppUser AppUser { get; set; }
     }
 }
