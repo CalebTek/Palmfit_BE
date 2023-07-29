f3341e0b4bdc6ff7245bc35f8f770adebfff6114
Mistaweng <122454766+Mistaweng@users.noreply.github.com>
2023-07-29 16:26:54 +0100

    Get all meals API added to the FoodController

--- Palmfit/Palmfit.Api/Controllers/FoodController.cs
+++ Palmfit/Palmfit.Api/Controllers/FoodController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Palmfit.Data.AppDbContext;
+using Palmfit.Data.Entities;
 
 namespace Palmfit.Api.Controllers
 {
@@ -8,6 +10,19 @@ namespace Palmfit.Api.Controllers
     public class FoodController : ControllerBase
     {
         
+        private readonly PalmfitDbContext _palmfitDb;
+
+        public FoodController(PalmfitDbContext palmfitDb)
+        {
+            _palmfitDb = palmfitDb;
+        }
+
+        [HttpGet("Get All Meals")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<IEnumerable<Food>> GetAllMeals()
+        {      
+            return Ok(_palmfitDb.Foods.ToList());
+        }
 
     }
 }
