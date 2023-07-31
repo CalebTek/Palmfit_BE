4d2d7282be6e0e79943c766bec661bd45d83c279
Kenneth Edoho <131462290+Kenneth075@users.noreply.github.com>
2023-08-01 00:23:32 +0100

    Created FoodClassDTO

---
+++ Palmfit/Palmfit.Core/Dtos/FoodClassDTO.cs
@@ -0,0 +1,18 @@
+﻿using Palmfit.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palmfit.Core.Dtos
+{
+    public class FoodClassDTO
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Details { get; set; }
+        public string Day { get; set; }
+        public ICollection<FoodDTO> Foods { get; set; }
+    }
+}
