3c24be3804c4d5c086fd0d7816196e7723d0ee2d
Unekwu001 <tusedor360@gmail.com>
2023-07-31 18:43:10 +0100

    Created AuthController and Login Api

---
+++ Palmfit/Palmfit.Api/Controllers/AuthController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Palmfit.Core.Dtos;
+using Palmfit.Core.Services;
+using Palmfit.Data.Entities;
+
+namespace Palmfit.Api.Controllers
+{
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository _authRepo;
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly IConfiguration _configuration;
+
+        public AuthController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, IConfiguration configuration, IAuthRepository authRepo)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _configuration = configuration;
+            _authRepo = authRepo;
+        }
+
+
+
+
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginDto login)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(new ApiResponse<string>("Invalid request. Please provide a valid email and password."));
+            }
+            else
+            {
+                var user = await _userManager.FindByEmailAsync(login.Email);
+
+                if (user == null)
+                {
+                    return NotFound(new ApiResponse<string>("User not found. Please check your email and try again."));
+                }
+
+                var result = await _signInManager.CheckPasswordSignInAsync(user, login.Password, lockoutOnFailure: false);
+
+                if (!result.Succeeded)
+                {
+                    return Unauthorized(new ApiResponse<string>("Invalid credentials. Please check your email or password and try again."));
+                }
+                else
+                {
+                    var token = _authRepo.GenerateJwtToken(user);
+
+                    // Returning the token in the response headers
+                    Response.Headers.Add("Authorization", "Bearer " + token);
+
+                    return Ok(new ApiResponse<string>("Login successful."));
+
+                }
+            }
+        }
+
+
+    }
+}
