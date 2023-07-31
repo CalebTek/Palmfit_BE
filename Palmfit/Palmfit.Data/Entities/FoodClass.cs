20265ebfd8e3996602be7376b356018770861a39
Kenneth Edoho <131462290+Kenneth075@users.noreply.github.com>
2023-08-01 00:32:24 +0100

    Uncommitted Changes Resolved

--- Palmfit/Palmfit.Data/Entities/FoodClass.cs
+++ Palmfit/Palmfit.Data/Entities/FoodClass.cs
@@ -5,6 +5,7 @@
         public string Name { get; set; }
         public string Description { get; set; }
         public string Details { get; set; }
+        public string Day { get; set; }
         public ICollection<Food> Foods { get; set; }
     }
 }
