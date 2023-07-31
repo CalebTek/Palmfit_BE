fb73598e9d9ba593bea6583a770ec1b4131a4282
Kenneth Edoho <131462290+Kenneth075@users.noreply.github.com>
2023-08-01 00:29:20 +0100

    Added FoodDTO

---
+++ Palmfit/Palmfit.Core/Dtos/FoodDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Palmfit.Data.Entities;
+
+namespace Palmfit.Core.Dtos
+{
+    public class FoodDTO
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Details { get; set; }
+        public string Origin { get; set; }
+        public string Image { get; set; }
+        public decimal Calorie { get; set; }
+        public string Unit { get; set; }
+        public string FoodClassId { get; set; }
+        //public FoodClass FoodClass { get; set; }
+    }
+}
