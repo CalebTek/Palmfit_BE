1b7f1c000963ef9458be987d3053874c6a8ecd56
Kenneth Edoho <131462290+Kenneth075@users.noreply.github.com>
2023-08-01 00:22:52 +0100

    Added MealPlanController API

---
+++ Palmfit/Palmfit.Api/Controllers/MealPlanController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Palmfit.Core.Dtos;
+using Palmfit.Core.Services;
+using Palmfit.Data.Entities;
+
+namespace Palmfit.Api.Controllers
+{
+    [Route("api/MealPlanController")]
+    [ApiController]
+    public class MealPlanController : ControllerBase
+    {
+        private readonly IMealPlanRepository _repository;
+        public MealPlanController(IMealPlanRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpPost("Add-MealPlan")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> AddMealPlan([FromBody]FoodClassDTO foodClassDTO)
+        {
+            var createMealResponse = _repository.AddMealPlan(foodClassDTO);
+
+            if (foodClassDTO == null)
+            {
+                return BadRequest(ApiResponse.Failed(foodClassDTO));
+            }
+
+            //return Ok();
+            //return CreatedAtAction("GetMealPlan", foodClassDTO);
+            return Ok(ApiResponse.Success(foodClassDTO));
+        }
+    }
+}
