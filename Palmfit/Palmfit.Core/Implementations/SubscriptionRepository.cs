b05e50016b8c8449586c08900af9043042e5f823
Unekwu001 <unekwutheophilus@gmail.com>
2023-08-05 16:14:15 +0100

    created endpoint to get user subscription status

---
+++ Palmfit/Palmfit.Core/Implementations/SubscriptionRepository.cs
@@ -0,0 +1,40 @@
+﻿using Palmfit.Core.Dtos;
+using Palmfit.Core.Services;
+using Palmfit.Data.AppDbContext;
+using Palmfit.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palmfit.Core.Implementations
+{
+    public class SubscriptionRepository : ISubscriptionRepository
+    {
+        private readonly PalmfitDbContext _palmfitDb;
+
+        public SubscriptionRepository(PalmfitDbContext palmfitDb)
+        {
+            _palmfitDb = palmfitDb;
+        }
+
+        public async Task<Subscription> CreateSubscriptionAsync(CreateSubscriptionDto subscriptionDto)
+        {
+            var subscription = new Subscription
+            {
+                Id = Guid.NewGuid().ToString(),
+                Type = subscriptionDto.Type,
+                StartDate = subscriptionDto.StartDate,
+                EndDate = subscriptionDto.EndDate,
+                AppUserId = subscriptionDto.AppUserId
+            };
+
+            _palmfitDb.Subscriptions.Add(subscription);
+            await _palmfitDb.SaveChangesAsync();
+
+            return subscription;
+        }
+    }
+
+}
