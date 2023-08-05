d1599b2601238e26011965c088b411f251357f0e
Mistaweng <122454766+Mistaweng@users.noreply.github.com>
2023-08-05 14:14:20 +0100

    Added an IUserRepository for update users

--- Palmfit/Palmfit.Core/Services/IUserInterfaceRepository.cs
+++ Palmfit/Palmfit.Core/Services/IUserInterfaceRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Data.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,8 @@ using System.Threading.Tasks;
 
 namespace Palmfit.Core.Services
 {
-    internal class IUserInterfaceRepository
+    public interface IUserInterfaceRepository
     {
+        Task<string> UpdateUserAsync(string id, UserDto userDto);
     }
 }
