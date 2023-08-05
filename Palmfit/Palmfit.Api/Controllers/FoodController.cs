d3b9b55a4a542f6abb5ce4e4bf1af293d74a38d8
Finejojo <108051271+Finejojo@users.noreply.github.com>
2023-08-05 23:06:22 +0100

    code conflict resolved

--- Palmfit/Palmfit.Api/Controllers/FoodController.cs
+++ Palmfit/Palmfit.Api/Controllers/FoodController.cs
@@ -46,14 +46,17 @@ namespace Palmfit.Api.Controllers
         public async Task<ActionResult<ApiResponse<FoodClassDto>>> UpdateFoodClass(string foodClassId, [FromBody] FoodClassDto updatedFoodClassDto)
         {
             var response = await _food.UpdateFoodClass(foodClassId, updatedFoodClassDto);
-            if(response == null)
+            if (response == null)
             {
                 return BadRequest(ApiResponse.Failed(response));
             }
             return Ok(ApiResponse.Success(response));
+        }
+
         /* < Start----- required methods to Calculate Calorie -----Start > */
 
         [HttpGet("calculate-calorie-by-name")]
+
         public async Task<ActionResult<ApiResponse<decimal>>> CalculateCalorieForFoodByName(string foodName, UnitType unit, decimal amount)
         {
             try
