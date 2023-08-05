48a5db5f2d729ab249e7e1d71f49fbb79b8b041f
Mistaweng <122454766+Mistaweng@users.noreply.github.com>
2023-08-05 14:06:59 +0100

    Added UserRepository

---
+++ Palmfit/Palmfit.Core/Services/IUserInterfaceRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palmfit.Core.Services
+{
+    internal class IUserInterfaceRepository
+    {
+    }
+}
