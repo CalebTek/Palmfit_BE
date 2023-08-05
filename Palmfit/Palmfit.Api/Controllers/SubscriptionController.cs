499b19d0077196cc24c5b87aa771553f1684e656
Unekwu001 <unekwutheophilus@gmail.com>
2023-08-05 14:31:09 +0100

    committed all

---
+++ Palmfit/Palmfit.Api/Controllers/SubscriptionController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Palmfit.Core.Dtos;
+using Palmfit.Core.Implementations;
+using Palmfit.Core.Services;
+using Palmfit.Data.Entities;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Palmfit.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SubscriptionController : ControllerBase
+    {
+        private readonly ISubscriptionRepository _subscriptionRepo;
+        private readonly UserManager<AppUser> _userManager;
+
+        public SubscriptionController(ISubscriptionRepository subscriptionRepo, UserManager<AppUser> userManager)
+        {
+            _subscriptionRepo = subscriptionRepo;
+            _userManager = userManager;
+        }
+
+
+
+        [HttpPost("create")]
+        public async Task<IActionResult> CreateSubscription([FromBody] CreateSubscriptionDto subscriptionDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ApiResponse.Failed("Invalid subscription data."));
+            }
+             
+            // Ensuring the user exists
+            var user = await _userManager.FindByIdAsync(subscriptionDto.AppUserId);
+            if (user == null)
+            {
+                return NotFound(ApiResponse.Failed("User not found."));
+                
+            }
+
+            var createdSubscription = await _subscriptionRepo.CreateSubscriptionAsync(subscriptionDto);
+            if (createdSubscription != null)
+            {
+                return Ok(ApiResponse.Success("Subscription created successfully."));
+            }
+            else
+            {
+                return BadRequest(ApiResponse.Failed("Subscription creation failed."));
+            }
+        }
+
+        
+
+    }
+}
+
---
+++ Palmfit/Palmfit.Core/Implementations/SubscriptionRepository.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Palmfit.Core.Dtos;
+using Palmfit.Core.Services;
+using Palmfit.Data.AppDbContext;
+using Palmfit.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palmfit.Core.Implementations
+{
+    public class SubscriptionRepository : ISubscriptionRepository
+    {
+        private readonly PalmfitDbContext _palmfitDb;
+
+        public SubscriptionRepository(PalmfitDbContext palmfitDb)
+        {
+            _palmfitDb = palmfitDb;
+        }
+
+        public async Task<Subscription> CreateSubscriptionAsync(CreateSubscriptionDto subscriptionDto)
+        {
+            var subscription = new Subscription
+            {
+                Id = Guid.NewGuid().ToString(),
+                Type = subscriptionDto.Type,
+                StartDate = subscriptionDto.StartDate,
+                EndDate = subscriptionDto.EndDate,
+                AppUserId = subscriptionDto.AppUserId
+            };
+
+            _palmfitDb.Subscriptions.Add(subscription);
+            await _palmfitDb.SaveChangesAsync();
+
+            return subscription;
+        }
+
+
+
+        public async Task<Subscription> GetUserSubscriptionStatusAsync(string userId)
+        {
+            return await _palmfitDb.Subscriptions.Where(sub => sub.AppUserId == userId).OrderByDescending(sub => sub.StartDate).FirstOrDefaultAsync();
+        }
+
+    }
+
+}
