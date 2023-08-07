e170edb6ca777d2d743361434f4e70a3a70dd4b0
James Adah <adahjames11@gmail.com>
2023-08-07 16:36:26 +0100

    added UserInfoDto

---
+++ Palmfit/Palmfit.Core/Dtos/UserInfoDto.cs
@@ -0,0 +1,26 @@
+﻿using Palmfit.Data.Entities;
+using Palmfit.Data.EntityEnums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palmfit.Core.Dtos
+{
+    public class UserInfo
+    {
+       string
+        public bool? IsLockedOut { get; set; }
+        public DateTime? LastOnline { get; set; }
+        public bool? IsVerified { get; set; }
+        public bool? IsArchived { get; set; }
+        public bool? Active { get; set; }
+        public string? ReferralCode { get; set; }
+        public string? InviteCode { get; set; }
+        public Health? Health { get; set; }
+        public Setting? Setting { get; set; }
+        public Wallet? Wallet { get; set; }
+
+    }
+}
