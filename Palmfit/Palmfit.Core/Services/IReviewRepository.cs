46b7f9b9786585d41b8659bb866dfe87f2ee8644
Unekwu001 <unekwutheophilus@gmail.com>
2023-08-07 03:12:43 +0100

    Created endpoint to get all reviews

---
+++ Palmfit/Palmfit.Core/Services/IReviewRepository.cs
@@ -0,0 +1,17 @@
+﻿using Palmfit.Core.Dtos;
+using Palmfit.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palmfit.Core.Services
+{
+    public interface IReviewRepository
+    {
+        Task<Review> AddReviewAsync(AddReviewDto reviewDto);
+        Task<string> DeleteReviewAsync(string userId, string reviewId);
+        Task<List<Review>> GetAllReviewsAsync();
+    }
+}
