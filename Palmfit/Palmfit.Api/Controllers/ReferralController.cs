a5bd791acff8e800dd92fda2d5ff26388916a85c
CalebTek <olawale.odeyemi@decagon.dev>
2023-08-08 15:53:42 +0100

    Create ReferralController.cs
    Add the endpoint to get invite by referral code

---
+++ Palmfit/Palmfit.Api/Controllers/ReferralController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Palmfit.Core.Dtos;
+using Palmfit.Core.Services;
+
+namespace Palmfit.Api.Controllers
+{
+    public class ReferralController : ControllerBase
+    {
+        private readonly IReferralRepository _referralRepository;
+        public ReferralController(IReferralRepository referralRepository) 
+        {
+            _referralRepository = referralRepository;
+        }
+
+        [HttpGet("get-invites-by-referral-code")]
+        public async Task<ActionResult<IEnumerable<InviteDto>>> GetInvitesByReferralCode(string referralCode)
+        {
+            if (string.IsNullOrWhiteSpace(referralCode))
+            {
+                return BadRequest(ApiResponse.Failed("Referral code cannot be empty."));
+            }
+
+            var invites = await _referralRepository.GetInvitesByReferralCodeAsync(referralCode);
+
+            return Ok(ApiResponse.Success(invites));
+        }
+
+    }
+}
