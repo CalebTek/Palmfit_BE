8f5c8370d9941393604646ed0e0788d0ddbb2c04
Dominic Precious <131398262+Nditech14@users.noreply.github.com>
2023-08-08 13:25:05 +0100

    created a walletcontroller and implemented HTTP Post to fund wallet

---
+++ Palmfit/Palmfit.Core/Implementations/WalletRepository.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using Palmfit.Core.Services;
+using Palmfit.Data.AppDbContext;
+using Palmfit.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palmfit.Core.Implementations
+{
+    public class WalletRepository :IWalletRepository
+    {
+
+        private readonly List<Wallet> _wallets = new List<Wallet>();
+        private readonly PalmfitDbContext _db;
+
+        public WalletRepository(PalmfitDbContext db)
+        {
+            _db = db;
+        }
+
+        public Wallet GetWalletByUserId(string userId)
+        {
+            return _wallets.SingleOrDefault(w => w.AppUserId == userId);
+        }
+
+        public async Task<string> RemoveFundFromWallet(string walletId, decimal amount)
+        {
+            var wallet = await _db.Wallets.FirstOrDefaultAsync(w => w.Id == walletId);
+
+            if (wallet == null)
+            {
+                return "Wallet not found"; // Wallet not found
+            }
+
+            if (wallet.Balance < amount)
+            {
+                return "Insufficient Balance, Please fund your wallet"; // Insufficient funds
+            }
+
+            wallet.Balance -= amount;
+            return "Successfully removed fund"; // Successfully removed funds
+        }
+    }
+}
---
+++ Palmfit/Palmfit.Core/Services/IWalletRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palmfit.Core.Services
+{
+    public interface IWalletRepository
+    {
+        Task<string> RemoveFundFromWallet(string walletId, decimal amount);
+    }
+}
