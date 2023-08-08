6a39722e3bb549f1d521239adc77a90a7e6966b5
zikglobal <125207118+zikglobal@users.noreply.github.com>
2023-08-08 21:43:41 +0100

    Resolving build errors

--- Palmfit/Palmfit.Data/AppDbContext/PalmfitDbContext.cs
+++ Palmfit/Palmfit.Data/AppDbContext/PalmfitDbContext.cs
@@ -1,4 +1,4 @@
-﻿using Data.Entities;
+﻿//using Data.Entities;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
@@ -25,7 +25,7 @@ namespace Palmfit.Data.AppDbContext
         public DbSet<AppUserPermission> AppUserPermissions  { get; set; }
         public DbSet<AppUserRole> AppUserRoles { get; set; }
         public DbSet<AppUser> users { get; set; }
-        public DbSet<UserOTP> UserOTPs { get; set; }
+        
 
 
         public PalmfitDbContext(DbContextOptions<PalmfitDbContext> options) : base(options)
