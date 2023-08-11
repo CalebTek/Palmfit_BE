199a417163e42ac134a3d7616acfce6b41b87c85
Onuh-Chuks <onuhchukwuma12@gmail.com>
2023-08-11 22:26:31 +0100

    Resloved build error

--- Palmfit/Palmfit.Api/Controllers/ReviewController.cs
+++ Palmfit/Palmfit.Api/Controllers/ReviewController.cs
@@ -14,7 +14,6 @@ namespace Palmfit.Api.Controllers
     public class ReviewController : ControllerBase
     {
         private readonly IReviewRepository _reviewRepository;
-        public ReviewController(IReviewRepository reviewRepository)
         private readonly IReviewRepository _reviewRepo; 
         private readonly UserManager<AppUser> _userManager;
 
@@ -30,30 +29,26 @@ namespace Palmfit.Api.Controllers
         public async Task<IActionResult> DeleteReview(string reviewId)
         {
             try
-        {
-            _reviewRepository = reviewRepository;
+            {
                 var loggedInUser = HttpContext.User;
                 var message = await _reviewRepo.DeleteReviewAsync(loggedInUser, reviewId);
 
                 return Ok(ApiResponse.Success(message));
-        }
-        [HttpGet("get-review-by-user/{userId}")]
-        [Authorize(Roles = "Admin")]
-        public async Task<ActionResult<List<ReviewDto>>> GetReviewsByUserId(string userId)
+            }
             catch (Exception ex)
-        {
-            var result = await _reviewRepository.GetReviewsByUserIdAsync(userId);
-            return Ok(ApiResponse.Success(result));
+            {
                 return BadRequest(ApiResponse.Failed(null, "An error occurred while deleting the review.", new List<string> { ex.Message }));
             }
         }
 
-
-
-
-
-
-
+        [HttpGet("get-review-by-user/{userId}")]
+        [Authorize(Roles = "Admin")]
+        public async Task<ActionResult<List<ReviewDto>>> GetReviewsByUserId(string userId)
+        {
+                var result = await _reviewRepository.GetReviewsByUserIdAsync(userId);
+                return Ok(ApiResponse.Success(result));
+        }
 
     }
 }
+
--- Palmfit/Palmfit.Core/Implementations/ReviewRepository.cs
+++ Palmfit/Palmfit.Core/Implementations/ReviewRepository.cs
@@ -16,36 +16,19 @@ namespace Palmfit.Core.Implementations
     public class ReviewRepository : IReviewRepository
     {
         private readonly PalmfitDbContext _dbContext;
-        public ReviewRepository(PalmfitDbContext dbContext)
         private readonly PalmfitDbContext _palmfitDb;
         private readonly UserManager<AppUser> _userManager;
 
         public ReviewRepository(PalmfitDbContext palmfitDb, UserManager<AppUser> userManager)  
         {
-            _dbContext = dbContext;
             _palmfitDb = palmfitDb;
             _userManager = userManager;
         }
 
         public async Task<List<Review>> GetReviewsByUserIdAsync(string userId)
-
-        public async Task<string> DeleteReviewAsync(ClaimsPrincipal loggedInUser, string reviewId)
         {
             var reviewsDto = new ReviewDto();
             var reviews = new Review
-            var user = loggedInUser.FindFirst(ClaimTypes.NameIdentifier);       
-            var review = await _palmfitDb.Reviews.FindAsync(reviewId);
-
-            string message = "";
-            if (user == null)
-            {
-                message= "User not found";
-            } 
-            else if(review.AppUserId != user.Value)
-            {
-                message = "You are not authorized to delete this review";
-            }
-            else if(review == null)
             {
                 Date = reviewsDto.Date,
                 Comment = reviewsDto.Comment,
@@ -55,21 +38,38 @@ namespace Palmfit.Core.Implementations
             };
             var ReviewResult = await _dbContext.Reviews.Where(r => r.AppUserId == userId).ToListAsync();
             if (!ReviewResult.Any())
+            {
+                return new List<Review>();
+            }
+            return ReviewResult;
+
+        }
+
+        public async Task<string> DeleteReviewAsync(ClaimsPrincipal loggedInUser, string reviewId)
+        {
+            var user = loggedInUser.FindFirst(ClaimTypes.NameIdentifier);
+            var review = await _palmfitDb.Reviews.FindAsync(reviewId);
+
+            string message = string.Empty;
+            if (user == null)
+            {
+                message = "User not found";
+            }
+            else if (review.AppUserId != user.Value)
+            {
+                message = "You are not authorized to delete this review";
+            }
+            else if (review == null)
+            {
                 message = "Review not found";
             }
             else
             {
-                return new List<Review>();
                 review.IsDeleted = true;
                 await _palmfitDb.SaveChangesAsync();
                 message = "Review deleted successful";
             }
             return message;
-            }
-            return ReviewResult;
-
-
-
         }
     }
 }
