e74389496b60b46c8dd951346d5a439f7223bfc8
Victor Ajibade <adesojimav@gmail.com>
2023-08-11 22:02:08 +0100

    merge conflict
    resolved merge conflict

--- Palmfit/Palmfit.Core/Dtos/ReviewDTO.cs
+++ Palmfit/Palmfit.Core/Dtos/ReviewDTO.cs
@@ -6,10 +6,12 @@ using System.Threading.Tasks;
 
 namespace Palmfit.Core.Dtos
 {
-	public class ReviewDTO
-	{
-		public string Comment { get; set; }
-		public int Rating { get; set; }
-		public string Verdict { get; set; }
-	}
+    public class ReviewDto
+    {
+        public DateTime Date { get; set; }
+        public string Comment { get; set; }
+        public int Rating { get; set; }
+        public string Verdict { get; set; }
+        public string AppUserId { get; set; }
+    }
 }
