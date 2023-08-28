bac22c66d93d15f8d4b754394ac248e739393646
CalebTek <olawale.odeyemi@decagon.dev>
2023-08-28 09:25:54 +0100

    Create AuthorizeMultiplePolicyFilter.cs
    Added AuthorizeMultiplePolicyFilter

---
+++ Palmfit/Palmfit.Infrastructure/Policies/AuthorizeMultiplePolicyFilter.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palmfit.Infrastructure.Policies
+{
+    public class AuthorizeMultiplePolicyFilter : IAsyncAuthorizationFilter
+    {
+        private readonly IAuthorizationService _authorization;
+        private string[] Policies { get; set; }
+        public AuthorizeMultiplePolicyFilter(string[] policies, IAuthorizationService authorization) 
+        {
+            Policies = policies;
+            _authorization = authorization;
+        }
+
+        public async Task OnAuthorizationAsync(AuthorizationFilterContext context)
+        {
+            foreach (var policy in Policies)
+            {
+                var authorized = await _authorization.AuthorizeAsync(context.HttpContext.User, policy);
+                if (authorized.Succeeded)
+                {
+                    return;
+                }
+            }
+            context.Result = new ForbidResult();
+            return;
+        }
+    }
+}
