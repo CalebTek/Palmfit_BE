6647eaaa73717118d0152862b9b7cefbbd33ea18
CalebTek <olawale.odeyemi@decagon.dev>
2023-08-28 09:12:55 +0100

    Added Admin Policy

---
+++ Palmfit/Palmfit.Infrastructure/Policies/Policies.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palmfit.Infrastructure.Policies
+{
+    public static class Policies
+    {
+        public const string Admin = "Admin";
+        public const string SuperAdmin = "SuperAdmin";
+
+        public static AuthorizationPolicy AdminPolicy()
+        {
+            return new AuthorizationPolicyBuilder()
+                .RequireAuthenticatedUser()
+                .RequireRole(Admin)
+                .Build();
+        }
+
+
+    }
+}
