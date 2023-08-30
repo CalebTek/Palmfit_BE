5a1702c87398e59bad0273a593cbf1e001f5ea8c
Victor Ajibade <adesojimav@gmail.com>
2023-08-30 16:47:59 +0100

    all changes

---
+++ Palmfit/Palmfit.Core/Dtos/MealPlanDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palmfit.Core.Dtos
+{
+	public class MealPlanDto
+	{
+		public int Day { get; set; }
+		public string MealOfDay { get; set; }
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public string Details { get; set; }
+		public string Origin { get; set; }
+		public string Image { get; set; }
+		public decimal Calorie { get; set; }
+		public string Unit { get; set; }
+		
+
+    }
+}
